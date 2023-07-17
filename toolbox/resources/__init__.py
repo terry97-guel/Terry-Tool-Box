7aee3f4a04f13ab8a81a204f721149386843788f
terry <taerimyoon@korea.ac.kr>
2023-07-17 17:13:26 +0900

    add menagerie assets

--- toolbox/resources/__init__.py
+++ toolbox/resources/__init__.py
@@ -2,45 +2,49 @@
 from toolbox import get_directory
 from pathlib import Path
 from pprint import pprint
+from enum import Enum
 
 RESOURCE_PATH = get_directory(__file__)
 
 ROBOT_PATH = RESOURCE_PATH / 'assets/robots'
+MANAGERIE_PATH = RESOURCE_PATH / 'assets/mujoco_menagerie'
 
-def get_robot_names():
-    path = ROBOT_PATH
-    names = []
-    for path_ in path.iterdir():
-        names.append(path_.name)
-    print(f'Available robots: {names}')
-    return names
+
     
+class ASSET_SRC(Enum):
+    CUSTOM = 0
+    MENAGERIE = 1
+
+def get_asset_from_name(name:str, TYPE=ASSET_SRC.CUSTOM, XML=True):
+    if TYPE is ASSET_SRC.CUSTOM:
+        path = ROBOT_PATH
+    elif TYPE is ASSET_SRC.MENAGERIE:
+        path = MANAGERIE_PATH
+    else:
+        raise ValueError(f"Unknown asset type {TYPE}")
     
-def get_asset_from_name(name:str):
-    path = ROBOT_PATH / name
+    if XML:
+        xml_dict = {}
+        for name in path.glob('**/*.xml'):
+            xml_dict[name.name] = name
+        
+        pprint(xml_dict)
+        return xml_dict
+    else:
+        urdf_dict = {}
+        for name in path.glob('**/*.urdf'):
+            urdf_dict[name.name] = name
+        pprint(urdf_dict)
+        return urdf_dict
 
-    robot_names = get_robot_names()
-    if name not in robot_names:
-        raise FileNotFoundError(f"Can not find {name} from robot names {robot_names}.")
-
-    xml_dict = {}
-    for name in path.glob('**/*.xml'):
-        xml_dict[name.name] = name
-    
-    urdf_dict = {}
-    for name in path.glob('**/*.urdf'):
-        urdf_dict[name.name] = name
-
-    asset_path = dict(xml=xml_dict, urdf=urdf_dict)
-    print(f"Available assets for {name}:")
-    pprint(asset_path)
-    return asset_path
+    # asset_path = dict(xml=xml_dict, urdf=urdf_dict)
+    # print(f"Available assets for {name}:")
+    # pprint(asset_path)
+    # return asset_path
 
 
 # %%
 if __name__ == '__main__':
-    robot_names = get_robot_names()
-    name = robot_names[0]
-
-    get_asset_from_name(name)
+    name = 'go1'
+    get_asset_from_name(name, ASSET_SRC.MENAGERIE)
     
